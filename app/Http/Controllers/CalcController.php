09faf76a5fd7936d8c24124d6cfdec957f6ada89
Gaurav Katiyar <gaurav.katiyar@mrmendtoend.com>
2019-05-18 14:49:09 +0530

    added google serp style display

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class CalcController extends Controller
 {
     public function calculate(Request $request){
-        echo "hello";die();
-        //return view('pixelhome');
+        //return response()->json(['message' => 'Thank you for contacting me.'], 201);
+        return response()->json($request, 201);
     }
 }
--- resources/views/pixelhome.blade.php
+++ resources/views/pixelhome.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <script src="{{URL::asset('js/jquery.min.js')}}"></script>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -17,21 +18,21 @@
                     <h2 class="bold">PixelCalculate</h2>
                 </div>
                 <div class="row">
-                    {{Form::open(['action' => 'CalcController@calculate', 'method'=> 'POST'])}}
+                    {{Form::open(['action' => 'CalcController@calculate', 'method' => 'POST', 'id' => 'calculateForm'])}}
                         <div class="col-md-6">
                             <div class="form-group">
-                                {{Form::text('title', '', ['id' => 'title', 'class' => 'form-control', 'placeholder' => 'Enter title here']) }}
+                                {{Form::text('title', '', ['id' => 'title', 'class' => 'form-control', 'placeholder' => 'Enter title here', 'autocomplete' => 'off']) }}
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                {{Form::text('url', '', ['id' => 'url', 'class' => 'form-control', 'placeholder' => 'Enter url here']) }}
+                                {{Form::text('url', '', ['id' => 'url', 'class' => 'form-control', 'placeholder' => 'Enter url here', 'autocomplete' => 'off']) }}
                             </div>
                         </div>
                         
                         <div class="col-md-12">
                             <div class="form-group">
-                                {{Form::textarea('description', '', ['class' => 'form-control', 'id' => 'description', 'cols' => 30, 'rows' => 7, 'placeholder' => 'Enter description here']) }}
+                                {{Form::textarea('description', '', ['class' => 'form-control', 'id' => 'description', 'cols' => 30, 'rows' => 7, 'placeholder' => 'Enter description here', 'autocomplete' => 'off']) }}
                             </div>
                         </div>
                         
@@ -44,6 +45,99 @@
                             <div class="form-group"  id="status"></div>
                         </div>
                 {{Form::close()}}
-        </div>
+            </div>
+            <div class="row">
+                <div class="g"><!--m-->
+                    <div data-hveid="CAAQAA" data-ved="2ahUKEwiL8azV1KTiAhWHuI8KHV0sCcgQFSgAMAF6BAgAEAA">
+                        <div class="rc">
+                            <div class="r">
+                                <a href="" id="responseUrlAnchor">
+                                    <h3 class="LC20lb" id="responseTitle"></h3><br>
+                                    <div class="TbwUpd">
+                                        <cite class="iUh30 bc" id="responseUrl"></cite>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="s">
+                                <div>
+                                    <span class="st" id="responseDescription"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!--n-->
+                </div>
+            </div>
+        <script>
+            function delay(callback, ms) {
+                var timer = 0;
+                return function() {
+                    var context = this, args = arguments;
+                    clearTimeout(timer);
+                    timer = setTimeout(function () {
+                    callback.apply(context, args);
+                    }, ms || 0);
+                };
+            }
+            jQuery('#calculateForm').keyup(delay(function (e){
+                e.preventDefault();
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('input[name="_token"]').attr('value')
+                    }
+                });
+                jQuery.ajax({
+                    url: "{{ url('calculate') }}",
+                    method: 'post',
+                    data: {
+                        title: jQuery('#title').val(),
+                        url: jQuery('#url').val(),
+                        description: jQuery('#description').val(),
+                    },
+                    success: function(data){
+                        if(data.title)
+                            $('#responseTitle').html(data.title);
+                        if(data.url){
+                            $('#responseUrl').html(data.url);
+                            //$('#responseUrlAnchor').replaceWith(data.url);
+                        }
+                        if(data.description)
+                            $('#responseDescription').html(data.description);
+                    },
+                    error: function(data){
+                        
+                    },
+                });
+            },300));
+            jQuery('#calculate').click(function (e){
+                e.preventDefault();
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('input[name="_token"]').attr('value')
+                    }
+                });
+                jQuery.ajax({
+                    url: "{{ url('calculate') }}",
+                    method: 'post',
+                    data: {
+                        title: jQuery('#title').val(),
+                        url: jQuery('#url').val(),
+                        description: jQuery('#description').val(),
+                    },
+                    success: function(data){
+                        if(data.title)
+                            $('#responseTitle').html(data.title);
+                        if(data.url){
+                            $('#responseUrl').replaceWith(data.url);
+                            //$('#responseUrlAnchor').replaceWith(data.url);
+                        }
+                        if(data.description)
+                            $('#responseDescription').html(data.description);
+                    },
+                    error: function(data){
+                        
+                    },
+                });
+            });
+        </script>
     </body>
 </html>
